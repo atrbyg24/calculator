e5dea5ffee476d9c356c081d9d79ce56ecc203f1
atrbyg24 <artwan520@gmail.com>
2025-01-16 14:33:00 -0500

    Write function for decimal button

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let firstNumber = "";
 let secondNumber = "";
-let operator = null;
-let firstNumberSign = true;
-let secondNumberSign = true;
+let operator = "";
+let result = "";
 
 function validOperation() {
     return (firstNumber !== "") && (secondNumber !== "") && (operator !== null);
@@ -27,34 +26,31 @@ function divide(a,b) {
 function populateDisplay() {
     let smallDisplay = document.querySelector(".small-display");
     let largeDisplay = document.querySelector(".large-display");
-    let a = firstNumberSign ? a : a * -1;
-    largeDisplay.textContent = a + operator;
+    smallDisplay.textContent = (result === "") ? "" : firstNumber + operator;
+    largeDisplay.textContent = (result === "") ? firstNumber + operator: result;
 }
 
 function operate() {
-    if (validOperation()) {
-        let a = parseFloat(firstNumber);
-        let b = parseFloat(secondNumber);
-        a = firstNumberSign ? a : a * -1;
-        b = secondNumberSign ? b : b * -1;
-        if (operator == "+") {
-            return add(a,b);
-        } else if (operator == "-") {
-            return subtract(a,b);
-        } else if (operator == "*") {
-            return multiply(a,b);
-        } else {
-            return divide(a,b);
-        }
+    let a = parseFloat(firstNumber);
+    let b = parseFloat(secondNumber);
+    a = firstNumberSign ? a : a * -1;
+    b = secondNumberSign ? b : b * -1;
+    if (operator == "+") {
+        return add(a,b);
+    } else if (operator == "-") {
+        return subtract(a,b);
+    } else if (operator == "*") {
+        return multiply(a,b);
+    } else {
+        return divide(a,b);
     }
 }
 
 function clear() {
     firstNumber = "";
     secondNumber = "";
-    operator = null;
-    firstNumberSign = true;
-    secondNumberSign = true;
+    operator = "";
+    result = "";
 }
 
 let btns = document.querySelector(".buttons-container");
@@ -62,21 +58,48 @@ let btns = document.querySelector(".buttons-container");
 btns.addEventListener("click", (event) => {
     let target = event.target;
     let numbers = ["zero","one","two","three","four","five","six","seven","eight","nine"];
-    if (target.id in numbers) {
-        if (operator === null) {
+    if (numbers.includes(target.id)) {
+        if (operator === "") {
             firstNumber += numbers.indexOf(target.id);
-            populateDisplay();
         } else {
             secondNumber += numbers.indexOf(target.id);
-            populateDisplay();
         }
+        populateDisplay();
     } else if (target.id === "AC") {
         clear();
+        populateDisplay();
     } else if (target.id === "plusminus") {
-        if (operator === null) {
-            if (firstNumber !== null) {
-                firstNumber = String(parseFloat(firstNumber) * -1);
+
+    } else if (target.id === "add") {
+
+    } else if (target.id === "subtract") {
+
+    } else if (target.id === "multiply") {
+
+    } else if (target.id === "divide") {
+
+    } else if (target.id === "delete") {
+
+        populateDisplay();
+    } else if (target.id === "decimal") {
+        if (firstNumber === "") {
+            firstNumber = "0.";
+        }
+        if (!(firstNumber === "") && operator === "" && secondNumber === "") {
+            if (!(firstNumber.includes("."))) {
+                firstNumber += ".";
             }
         }
+        if (!(firstNumber === "") && !(operator === "")) {
+            if (secondNumber === "") {
+                secondNumber = "0.";
+            }
+            if (!(secondNumber === "") && !(secondNumber.includes("."))) {
+                secondNumber += ".";
+            }
+        }
+        populateDisplay();
+    } else if (target.id === "equals") {
+
     }
 })
